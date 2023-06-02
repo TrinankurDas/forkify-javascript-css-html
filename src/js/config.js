3ceb3841f3d11172c4807ad947fd9db94abfc59b
TrinankurDas <hynotizedmole58@gmail.com>
2023-06-02 12:14:58 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'ab72397e-587d-4b1a-94c5-52a70db87b82';
+export const MODAL_CLOSE_SEC = 2.5;
